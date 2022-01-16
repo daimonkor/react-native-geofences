2be2716898e669c2a2ef6e34f08b2e195ce09047
Dima Korniets <dimakorniets@gmail.com>
2022-01-16 23:52:56 +0200

    Add interface for appdelegate

--- ios/Geofences-Bridging-Header.h
+++ ios/Geofences-Bridging-Header.h
@@ -1,2 +1,11 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTViewManager.h>
+
+@protocol GeofenceManagment
+- (void) startMonitoring: (RCTPromiseResolveBlock) resolve reject: (RCTPromiseRejectBlock) reject;
+- (void) stopMonitoring:  (RCTPromiseResolveBlock) resolve reject: (RCTPromiseRejectBlock) reject;
+@end
+
+@protocol GeofenceDelegate
+- (void) geofenceEvent:(NSDictionary *) geofenceModel geofenceManager: (id<GeofenceManagment>) geofenceManager;
+@end
