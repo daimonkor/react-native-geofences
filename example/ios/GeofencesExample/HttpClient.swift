d5c543c6067f4ba1643beb49d7be1a5715139c83
Dima Korniets <dimakorniets@gmail.com>
2022-01-17 00:27:12 +0200

    Add dataRequest method

--- example/ios/GeofencesExample/HttpClient.swift
+++ example/ios/GeofencesExample/HttpClient.swift
@@ -28,4 +28,36 @@ class HttpClient: NSObject {
   public static func  hello(){
     print("AAAAAA")
   }
+  
+  static func dataRequest<T: Decodable>(with url: String, headers: [String: Any?]?, body: [String: Any?]?, objectType: T.Type, completion: @escaping (Result<T>) -> Void) {
+         let dataURL = URL(string: url)!
+         let session = URLSession.shared
+         var request = URLRequest(url: dataURL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 60)
+         headers?.forEach({ (key: String, value: Any?) in
+             request.addValue(key, forHTTPHeaderField: value as? String ?? "")
+         })
+         let jsonData = try? JSONSerialization.data(withJSONObject: body)
+         if(jsonData != nil){
+             request.httpBody = jsonData
+         }
+         print("Request: \(request)")
+        
+         let task = session.dataTask(with: request, completionHandler: { data, response, error in
+             guard error == nil else {
+                 completion(Result.failure(AppError.networkError(error!)))
+                 return
+             }
+             guard let data = data else {
+                 completion(Result.failure(AppError.dataNotFound))
+                 return
+             }
+             do {
+                 let decodedObject = try JSONDecoder().decode(objectType.self, from: data)
+                 completion(Result.success(decodedObject))
+             } catch let error {
+                 completion(Result.failure(AppError.jsonParsingError(error as! DecodingError)))
+             }
+         })
+         task.resume()
+     }
 }
